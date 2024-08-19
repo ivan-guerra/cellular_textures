abd3444beac0a54456b43d863d5a8a1b5b9ca99f
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-08-18 22:57:55 -0400

    Move type definitions to separate header file.

--- include/Texture.h
+++ include/Texture.h
@@ -1,58 +1,12 @@
 #pragma once
 
-#include <array>
-#include <cstddef>
-#include <expected>
 #include <filesystem>
-#include <functional>
 #include <istream>
 #include <span>
-#include <vector>
+
+#include "Types.h"
 
 namespace ctext {
-
-struct Pixel;
-using PixelVect = std::vector<Pixel>;
-
-struct Point2D;
-using PointVect = std::vector<Point2D>;
-
-struct TextureConfig;
-using DistFunc = std::function<float(
-    const Pixel&, const std::span<const Point2D>, const TextureConfig&)>;
-
-constexpr size_t kTotalDistFuncs = 3;
-using DistFuncTable = std::array<DistFunc, kTotalDistFuncs>;
-
-enum DistMetric {
-  DistToNearestPoint = 0,
-  DistToNearestTwoPointsDelta,
-  DistToNearestTwoPointsProduct
-};
-
-struct Dimension2D {
-  size_t width = 0;
-  size_t height = 0;
-};
-
-struct TextureConfig {
-  Dimension2D dim;
-  size_t num_points = 0;
-  bool invert_colors = false;
-  bool is_tiled = false;
-  DistMetric metric = DistMetric::DistToNearestPoint;
-};
-
-struct Pixel {
-  size_t row = 0;
-  size_t col = 0;
-  float color = 0.f;
-};
-
-struct Point2D {
-  float x = 0.f;
-  float y = 0.f;
-};
 
 namespace distfunc {
 
